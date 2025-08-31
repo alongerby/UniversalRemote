c0827c9c7fc6c4d945400c2028f51c5c23ae3520
Alon Gerby <alongerby007@gmail.com>
2025-08-31 18:43:35 +0300

    simple on off - ac

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,58 @@
 #include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <ir_Gree.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const uint16_t kIrTxPin = 4;   // DAT pin
+IRGreeAC ac(kIrTxPin);
+
+String line;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(2000); // let the port settle
+  ac.begin();
+  Serial.println("\nReady. Type ON or OFF, then press Enter.");
+}
+
+void sendState() {
+  ac.send();
+  Serial.println("IR sent.");
+}
+
+void processCmd(const String& s) {
+  if (s == "ON") {
+    ac.on();
+    sendState();
+  } else if (s == "OFF") {
+    ac.off();
+    sendState();
+  } else {
+    Serial.println("Unknown. Use ON or OFF.");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while (Serial.available()) {
+    char c = (char)Serial.read();
+    Serial.print(c);
+    if (c == '\r') {
+      continue;
+    }
+    if(c == '\n'){
+      line.trim();
+      if(line == "ON"){
+        Serial.print("HERE");
+        Serial.println("ON");
+        processCmd(line);
+      }
+      if(line == "OFF"){
+        Serial.println("OFF");
+        processCmd(line);
+      }
+      line = "";
+    }else{
+      line += c;
+    }
+  }
 }
